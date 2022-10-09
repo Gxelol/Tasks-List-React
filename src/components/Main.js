33b6c1a272276bbef41ab745097399b55e7abf98
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-09 20:45:59 -0300

    Components

---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './Main.css'
+
+export default class Main extends Component {
+  state = {
+    newTask: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      newTask: e.target.value,
+    });
+  }
+
+  render() {
+    // const { newTask } = this.state;
+
+    return (
+      <div className='main'>
+        <h1>Tasks List</h1>
+
+        <form action="#">
+          <input onChange={this.handleChange}type="text" />
+          <button type='submit'>Send</button>
+        </form>
+      </div>
+    )
+  }
+}
