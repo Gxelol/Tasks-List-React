5b3ec5a9ecbe825c88b3729428c677bd6909d35d
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-10 17:56:50 -0300

    saving to localStorage

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,6 +16,22 @@ export default class Main extends Component {
     index: -1,
   };
 
+  componentDidMount() {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    if (!tasks) return;
+
+    this.setState({ tasks });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tasks } = this.state;
+
+    if (tasks === prevState.tasks) return;
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }
+
   handleDelete = (e, index) => {
     const { tasks } = this.state;
     const  deleteTasks  = [...tasks];
